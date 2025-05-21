ec11582f362f04536388857490ef2194173986c6
rishikisgood <Kaulrishik15@gmail.com>
2025-05-21 17:03:23 +0530

    Add files via upload

---
+++ Prder Precedence.py
@@ -0,0 +1,15 @@
+V=6
+W=7
+X=1
+Y=4
+Z=0
+Z=(V+W) * X /Y;
+print("Value of (V=W) *X / y is:", Z)
+
+
+Name="Alex"
+Age=0
+if Name=="Alex" or Name== "John" and Age>=2:
+    print("Hello! Welcome.")
+else:
+    print("Good Bye!!")
